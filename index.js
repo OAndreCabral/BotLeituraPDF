1f8966a620ac7cb5233f3c3ea885384ad17856af
andre.cabral <andre.cabral@db1.com.br>
2024-07-18 09:38:47 -0300

    bot de leitura

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const readline = require("readline");
 const fs = require("fs");
 
 const line = readline.createInterface({
-    input: fs.createReadStream("teste.pdf")
+    input: fs.createReadStream("./livros/hp.pdf")
 });
 
 line.on("line", (data) => {
-    let pdf = data.split(";")
+    let pdf = data.split(",")
     console.log('====================================');
     console.log(pdf);
     console.log('====================================');
